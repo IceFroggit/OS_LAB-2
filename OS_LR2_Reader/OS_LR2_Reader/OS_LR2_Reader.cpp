727fad07c5e32cdeeed15104c3d86d336656c3da
Adil <81522631+IceFroggit@users.noreply.github.com>
2022-12-28 11:38:08 +0300

    Add files via upload

---
+++ OS_LR2_Reader/OS_LR2_Reader/OS_LR2_Reader.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+void Reader()
+{
+	setlocale(LC_ALL, "Russian");
+	string fileName; 
+	HANDLE HandleMap; 
+	LPVOID Memory; 
+	cout << "Введите имя файла: ";
+	cin >> fileName;
+	HandleMap = OpenFileMappingA(FILE_MAP_READ | FILE_MAP_WRITE, FALSE, fileName.c_str());
+	Memory = MapViewOfFile(HandleMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);//проецирование фрагмента файла в память
+	if (Memory)
+	{
+		cout << "Содержимое файла " << endl;
+		cout << (char*)Memory << endl;
+		cout << "Для завершения введите любой символ: ";
+		cin >> fileName;
+		UnmapViewOfFile(Memory); //завершение проецирования файла
+	}
+	else
+	{
+		cout << "Ошибка указан не верный путь или файл не существует";
+	}
+}
+
+int main()
+{
+	Reader();
+	return 0;
+}
